85fb9dd2c95ef232e267002f4e70d8fe3dcf5f03
Sophie Kim <270010565@bun2-206-25.local>
2019-10-29 12:23:57 +1300

    rendering all templates

--- templates/content-audio.php
+++ templates/content-audio.php
@@ -1,6 +1,21 @@
+<?php
+if (has_blocks()) {
+    $allBlocks = parse_blocks(get_the_content());
+    for ($i=0; $i < count($allBlocks); $i++) {
+        if ($allBlocks[$i]['blockName'] === 'core/audio' ) {
+            $firstAudioBlock = $allBlocks[$i];
+            break;
+        }
+    }
+}
+ ?>
+
 <div class="card h-100 border border-success">
     <div class="card-body">
         <h5 class="card-title"><?php the_title(); ?></h5>
+        <div class="fullAudio">
+            <?php echo apply_filters('the_content', render_block($firstAudioBlock)) ; ?>
+        </div>
         <a href="<?php the_permalink(); ?>" class="btn btn-success btn-block">Listen to Audio</a>
     </div>
 </div>
--- templates/content-image.php
+++ templates/content-image.php
@@ -1,6 +1,21 @@
+<?php
+if (has_blocks()) {
+    $allBlocks = parse_blocks(get_the_content());
+    for ($i=0; $i < count($allBlocks); $i++) {
+        if ($allBlocks[$i]['blockName'] === 'core/image' ) {
+            $firstImageBlock = $allBlocks[$i];
+            break;
+        }
+    }
+}
+ ?>
+
 <div class="card h-100 border border-info">
     <div class="card-body">
         <h5 class="card-title"><?php the_title(); ?></h5>
+        <div class="img-fluid">
+            <?php echo apply_filters('the_content', render_block($firstImageBlock)) ; ?>
+        </div>
         <a href="<?php the_permalink(); ?>" class="btn btn-info btn-block">View Image</a>
     </div>
 </div>
