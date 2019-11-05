aa76dfd22befd4ae53b77fd32530a60c07a19568
Sophie Kim <270010565@bun2-206-25.local>
2019-11-06 09:30:30 +1300

    remove unnecessary file

--- video.php
+++
@@ -1,6 +0,0 @@
-
-<?php array(1) { [0]=> array(5) { ["blockName"]=> string(18) "core-embed/youtube" ["attrs"]=> array(4) { ["url"]=> string(43) "https://www.youtube.com/watch?v=SlPhMPnQ58k" ["type"]=> string(5) "video" ["providerNameSlug"]=> string(7) "youtube" ["className"]=> string(40) "wp-embed-aspect-16-9 wp-has-aspect-ratio" } ["innerBlocks"]=> array(0) { } ["innerHTML"]=> string(228) "
-https://www.youtube.com/watch?v=SlPhMPnQ58k
-" ["innerContent"]=> array(1) { [0]=> string(228) "
-https://www.youtube.com/watch?v=SlPhMPnQ58k
-" } } } ?>
